548030a706c7320ccaac7bc6b5fb34bd7e3fa638
Christer G. Sørensen <15308761+texnevada@users.noreply.github.com>
2026-06-19 15:25:05 +0200

    Updated website with more info on the extension

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ import io
 if not Path("env.ini").exists():
     debug = False
     host = "0.0.0.0"
-    port = 5000 # Linux no happy with port 80 :(
+    port = 80 # Borked it in dev. Reverting back
 else:
     config = configparser.ConfigParser()
     config.read("env.ini")
